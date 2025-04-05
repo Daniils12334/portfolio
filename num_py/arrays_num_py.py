d9c745556a2913cc71dce0a73d5b45615d169646
Daniils12334 <tmsthe4@gmail.com>
2025-04-05 12:23:17 +0300

    xxx

---
+++ num_py/arrays_num_py.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# a = [1,2,3,4,5]
+# print(a)
+# print(a[1:4])
+# print(a[-1])
+
+# a = np.array([1,2,3,4,5])
+# print(a)
+# print(a[:-2])
+# print(a[1])
+# print(a[1:])
+# print(type(a))
+
+# a[2] = 10
+# print(a)
+
+# a_mul = np.array([[[1,2,3],[4,5,6],[7,8,9],[10,11,12]],[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]]) 
+# print(a_mul.shape) #output (2, 4, 3) 2 arrays, 4 arrays, 3 elements in each
+# print(a_mul.ndim) # list in list in list/// deepnes
+# print(a_mul.size) # 2 * 3 * 4
+# print(a_mul.dtype) # data type
+
--- python_basics/py_qt5/qt_intro.py
+++ python_basics/py_qt5/qt_intro.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
 from PyQt5.QtCore import Qt
 
 
@@ -15,6 +15,12 @@ class MainWindow(QMainWindow):
         label.setFont(QFont("Arial", 40))
         label.setGeometry(0,0, 500, 100)
         label.setStyleSheet("color:000000; background-color:#6fdcf7; font-weight:bold; font-style;italic; text-decoration:underline;")
+        label1 = QLabel(self)
+        pixmap = QPixmap("/home/danbar/Desktop/project_x/istockphoto-502739469-612x612.jpg")
+        label1.setPixmap(pixmap)
+        label.setScaledContents(True)
+        label1.setGeometry(self.width() - label1.width(),self.height()-label1.height(),label1.width(),label1.height())
+
 
         label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # Right, VCenter, HCenter, Bottom, Top
 def main():
